87ef703e56139f6efb61b50a7198d4fcb5d4c47c
LydiaWinnie <lydia.100777@gmail.com>
2024-05-02 18:07:51 +0800

    20240502

---
+++ 20240502/ImageDemo/app/src/main/java/com/example/imagedemo/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.imagedemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private ImageView image1,image2,image3,image4;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        image1=(ImageView) findViewById(R.id.imgOutput);
+        image2=(ImageView) findViewById(R.id.imgOutput);
+        image3=(ImageView) findViewById(R.id.imgOutput);
+        image4=(ImageView) findViewById(R.id.imgOutput);
+        RadioGroup rg = (RadioGroup) findViewById(R.id.rgImages);
+        rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if (checkedId == R.id.rdbDog) {
+                  image1.setVisibility(ImageView.VISIBLE);
+                  image2.setVisibility(ImageView.GONE);
+                  image3.setVisibility(ImageView.GONE);
+                  image4.setVisibility(ImageView.GONE);
+                } else if (checkedId == R.id.rdbElephant) {
+                    image1.setVisibility(ImageView.GONE);
+                    image2.setVisibility(ImageView.VISIBLE);
+                    image3.setVisibility(ImageView.GONE);
+                    image4.setVisibility(ImageView.GONE);
+                } else if (checkedId == R.id.rdbMouse) {
+                    image1.setVisibility(ImageView.GONE);
+                    image2.setVisibility(ImageView.GONE);
+                    image3.setVisibility(ImageView.VISIBLE);
+                    image4.setVisibility(ImageView.GONE);
+                } else if (checkedId == R.id.rdbRabbit) {
+                    image1.setVisibility(ImageView.GONE);
+                    image2.setVisibility(ImageView.GONE);
+                    image3.setVisibility(ImageView.GONE);
+                    image4.setVisibility(ImageView.VISIBLE);
+
+                }
+            }
+
+        });
+    }
+}
